54540c377ccec7a9a5c077973cc51de1936a8f14
GuruVII <guruvii@gmail.com>
2016-07-04 16:14:56 +0200

    added checks for duplicates, added messages for failing and succeeding^Cslightly fixed the UI

--- src/js/script.js
+++ src/js/script.js
@@ -4,6 +4,10 @@ var coloursMasterList = ["red", "blue", "yellow", "orange", "brown", "green"]; /
 var computerSelectedColours = [];
 var numOfAttempts = 0;
 var allGuesses = [];
+var duplicate = false; //this variable will be used to test, if you entered a duplicate combination
+
+
+$("#try").prop("disabled", false ); //if the try button is disabled, this enables it
 
 //the computer selects the colours
 function selectColour() {
@@ -51,7 +55,7 @@ $(".colour").click(function(){ // this function individually moves colours in th
 
 
 $("#try").click(function() {
-	var guess = [];
+	var guess = []; // array that stores your current guess
 	var currentResult = { // object that will be used to store current correct guesses
 		correctColour: 0,
 		correctPlace: 0
@@ -62,42 +66,75 @@ $("#try").click(function() {
 	});
 	console.log(guess);
 
-	for (var i = 0; i < 4; i++) {
+	for (var i = 0; i < 4; i++) { //check, if the the computer selected colours match with the guess
 		for (var j = 0; j < 4; j++) {
-			if ((computerSelectedColours[i] == guess[j]) && (j == i)) {
+			if ((computerSelectedColours[i] == guess[j]) && (j == i)) { //checks if you guessed both the colour and position
 				currentResult.correctPlace++;
 				break;
 			};
-			if (computerSelectedColours[i] == guess[j]) {
+			if (computerSelectedColours[i] == guess[j]) { //checks if you guessed the colour but not the position
 				currentResult.correctColour++;
 				break;
 			}
 		}
-
 		if ( currentResult.correctPlace == 4){ //testing if you gussed all the colours corectly
-			console.log("congratulations, you've deduced the pattern")
+			numOfAttempts++ //adds an attempt becuase attempts are normally counted elsewhere
+			alert("congratulations, you've deduced the pattern, it took you " + numOfAttempts + " attempt(s)")
+			$("#try").prop("disabled", true );
 		}
+}
 
-		
+allGuesses.forEach(function(array) {
+	if (array.equals(guess)){//equals is a new method added using prototype, which is preseint in the all.js or if looking at src folder in the newPrototype.js
+		duplicate = true;
+	alert("you've are trying an existing combination")
+
+	};
+});
+
+function check() {
+	for (var i = 0; i < 4; i++) {
+		for (var j = 0; j < 4; j++) {
+			if ( (guess[i] == guess[j]) && (i != j)){ //checking if there are any duplicates inside the array
+				alert("You've used duplicate colours in your guess, try again")
+				return true
+			};
+		};
+	};
+	return false
+};
 
 
-
-	}
-
-	guess.forEach(function(currentValue){
-		$( "#results" ).append( "<div class='col-xs-1 rounded-border unselectable' style='background-color:"+currentValue+"; color:"+currentValue+"'>"+ currentValue + "</div>" );
-	});
+if ((duplicate == false) && (check() == false)){ //check, if the guess is a duplciate, if not, then it proceeds with the code
+guess.forEach(function(currentValue, index){ //adds your current guess to the page
+	if (index == 0){ //each guess should have its own row
+		$( "#results" ).append("<div class='row'>")
+	};
+	$( "#results" ).append( "<div class='col-xs-1 rounded-border unselectable' style='background-color:"+currentValue+"; color:"+currentValue+"'>"+ currentValue + "</div>" );
+	if (index == 3){ //each guess should have its own row
+		$( "#results" ).append("</div>")
+	};
+});
 
 	numOfAttempts++ // cunting the numbers of attemps
 	console.log(numOfAttempts);
 	if (numOfAttempts == 10){
-		console.log("you have used all your attempts");
+		alert("You've used all your attempts, the correct combination was " + computerSelectedColours[0] + ", " + computerSelectedColours[1] + ", " + computerSelectedColours[2] + ", "  + computerSelectedColours[3] );
+		$("#try").prop("disabled", true );
 		}
 	allGuesses.push(guess);//pushes this rounds gusses into a single array
 
 console.log(currentResult.correctColour);
 console.log(currentResult.correctPlace);
 console.log(allGuesses);
-})
+};
+duplicate = false;
+});
+
+$("#start").click(function() { //reloads the page
+
+	location.reload(); 
+});
+
 
 });
